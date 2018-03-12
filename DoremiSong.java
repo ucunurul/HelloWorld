27d2bd9a2c9b7867dde4979c6e82f5cb25ee0a4d
Putra Dwiyanto <photondev.prob@gmail.com>
2018-03-12 15:02:28 +0700

    added new line

--- DoremiSong.java
+++ DoremiSong.java
@@ -3,10 +3,13 @@ public class Doremi {
 
 	String[] Song = {"Doe A deer ,Female Deer.","Ray , A drop of golden sun.","Me , The name i call myself.","Far, A long long way to run.","Sew , A neddle pulling thread.","La , A note to follow So.","Tea , A Drink with jam and bread.","That will bring us back to DO.","DO RE MI FA SO LA TI DO."};
 
+	String Song2 = "Doe A deer ,Female Deer. \nRay , A drop of golden sun. \nMe , The name i call myself. \nFar, A long long way to run. \nSew , A neddle pulling thread. \nLa , A note to follow So. \nTea , A Drink with jam and bread. \nThat will bring us back to DO. \nDO RE MI FA SO LA TI DO.";
 	for (int i = 0;i<Song.length;i++){
 	
 	System.out.println(Song[i]);
 		}
+	System.out.println("\nNext... \n");
+	System.out.println(Song2);
 
 	}
 }
