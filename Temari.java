aa7ce49d6ded02d6f3ed6bd1720299220bfc476c
Putra Dwiyanto <photondev.prob@gmail.com>
2018-03-12 15:33:00 +0700

    Add new file

---
+++ Temari.java
@@ -0,0 +1,13 @@
+public class coba {
+    
+    public static void main(String []args){
+        int num = 15;
+        int mod = num % 2;
+        String modulo = mod == 0 ? "Genap" : "Ganjil";
+        
+        System.out.println("Angka " + mod +" Adalah " + modulo);
+        
+    }
+    
+    
+}
