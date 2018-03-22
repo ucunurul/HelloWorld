54639309e518318144d71614509952d5acbf08bd
Rihaf Husen <rihaf.husen@photoninfotech.net>
2018-03-22 11:12:10 +0700

    push to origin

--- DoremiSong.java
+++ DoremiSong.java
@@ -1,7 +1,7 @@
 public class Doremi {
 	public static void main(String[]args){
 
-	String[] Song = {"Doe A deer ,Female Deer.","Ray , A drop of golden sun.","Me , The name i call myself.","Far, A long long way to run.","Sew , A neddle pulling thread.","La , A note to follow So.","Tea , A Drink with jam and bread.","That will bring us back to DO.","DO RE MI FA SO LA SI DO."};
+	String[] Song = {"Doe A deer ,Female Deer.","Ray , A drop of golden sun.","Me , The name i call myself.","Far, A long long way to run.","Sew , A neddle pulling thread.","La , A note to follow So.","Tea , A Drink with jam and bread.","That will bring us back to DO.","DO RE MI FA SO LA TI DO."};
 
 	for (int i = 0;i<Song.length;i++){
 	
