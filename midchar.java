1f7804429eb086550df37aabf376d559bdcb3522
Rihaf Husen <rihaf.husen@photoninfotech.net>
2018-03-22 11:34:00 +0700

    added new java file

---
+++ midchar.java
@@ -0,0 +1,39 @@
+public class midchar1 {
+    
+    public static void main(String []args){
+        midchar(String str)
+        
+        Int position;
+        Int length;
+        
+        if(str.length() % 2 == 0{
+            position = str.length()/2-1;
+            Length = 2;
+        }
+           
+           Else {
+               
+               Position = str.length()/2;
+               Length = 1;
+               
+               Return str.substring(position, position + length);
+               
+               
+               Create function to read middle letter
+               
+               Mid char (String str){
+                   Int positon;
+                   Int length;
+                   
+                   if(str.length() % 2 == 0){
+                       Position = str.length()/2-1;
+                       Length = 2;
+                       
+                   }else
+                       Positon = str.length()/2;
+                   length=1;}
+               Return str.substring(position, position + length);
+               
+           }
+           }
+           }
