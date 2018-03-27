5eaffc813827c66650881bb6ed9c785d050afc7e
Rihaf Husen <rihaf.husen@photoninfotech.net>
2018-03-27 09:38:53 +0700

    add new file to thubranch

---
+++ secret stash/hakuna matata.java
@@ -0,0 +1,15 @@
+public class HakunaMatata {
+    public static void main(String[] args){
+        
+        String lyric[]={"Hakuna Matata, what a wonderful phrase...", 
+                        "Haukna Matata, ain't no passing craze...", 
+                        "It means no worries, for the rest of your day",
+                        "It's a problem free...","Philosophy.....",
+                           "Hakuna Matata"};
+        
+        for(int i=0; i<lyric.length; i++){
+            System.out.println(lyric[i]);
+                
+        }
+    }       
+}
