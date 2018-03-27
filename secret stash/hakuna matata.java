a6fc8585906e30ef96a46720eb751c458229af55
Rihaf Husen <rihaf.husen@photoninfotech.net>
2018-03-27 09:44:28 +0700

    hakuna matata edited to thubranch

--- secret stash/hakuna matata.java
+++ secret stash/hakuna matata.java
@@ -1,15 +1,20 @@
 public class HakunaMatata {
     public static void main(String[] args){
         
-        String lyric[]={"Hakuna Matata, what a wonderful phrase...", 
-                        "Haukna Matata, ain't no passing craze...", 
-                        "It means no worries, for the rest of your day",
-                        "It's a problem free...","Philosophy.....",
-                           "Hakuna Matata"};
-        
-        for(int i=0; i<lyric.length; i++){
-            System.out.println(lyric[i]);
+        String lyric[]={"Hakuna Matata, what a wonderful phrase...",
+            "Haukna Matata, ain't no passing craze...",
+            "It means no worries, for the rest of your day",
+            "It's a problem free...","Philosophy.....",
+            "Hakuna Matata"};
+        int x = 0;
+        while (x < 2){
+            for(int i=0; i<lyric.length; i++){
+                System.out.println(lyric[i]);
                 
+            }
+            System.out.println();
+            x++;
         }
-    }       
+    }
 }
+
