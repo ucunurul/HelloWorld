8b92d9b666b6e59b0ec204674661b378e208b2d9
Mirza Kemal Abdullah <mirzakemalabd@gmail.com>
2019-12-17 10:43:53 +0700

    tes switch

---
+++ javagit/src/javagit/TesSwitch.java
@@ -0,0 +1,19 @@
+package javagit;
+
+public class TesSwitch {
+
+	public static void main(String[] args) {
+		int day =5;
+		switch(day) {
+		case 1:
+			System.out.println("senin");
+			break;
+		case 2:
+			System.out.println("selasa");
+			break;
+		default:
+			System.out.println("not found");
+		}
+	}
+
+}
